38658306f5a4a4da20eab7772551df2e658ff6b1
zuguangz97 <zuguangzhu3@gmail.com>
2024-04-08 14:56:25 -0700

    add Package controller and Class HomeController

---
+++ springlive/src/main/java/net/nvsoftware/springlive/controller/HomeController.java
@@ -0,0 +1,14 @@
+package net.nvsoftware.springlive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "NVSoftware Home";
+    }
+}
