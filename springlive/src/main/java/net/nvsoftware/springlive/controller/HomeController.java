cfd42eaabdcb3a37a1194febbb08d3b1abaed10e
zuguangz97 <zuguangzhu3@gmail.com>
2024-04-08 15:23:26 -0700

    add User model, refactor to rescontroller, set up postman to test api

--- springlive/src/main/java/net/nvsoftware/springlive/controller/HomeController.java
+++ springlive/src/main/java/net/nvsoftware/springlive/controller/HomeController.java
@@ -1,14 +1,24 @@
 package net.nvsoftware.springlive.controller;
 
+import net.nvsoftware.springlive.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @ResponseBody
 public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "NVSoftware Home";
+    }
+    @RequestMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setId("3100");
+        user.setName("nvsoftware");
+        user.setEmail("info@nvsoftware.net");
+        return user;
     }
 }
