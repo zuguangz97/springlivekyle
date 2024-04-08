60bf3dcec15c4a042bd3ced313626ecc56996fd3
zuguangz97 <zuguangzhu3@gmail.com>
2024-04-08 14:27:42 -0700

    springone setup

---
+++ springlive/src/main/java/net/nvsoftware/springlive/SpringliveApplication.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.springlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringliveApplication.class, args);
+	}
+
+}
---
+++ springlive/src/test/java/net/nvsoftware/springlive/SpringliveApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.springlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
