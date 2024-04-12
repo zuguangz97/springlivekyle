90f156232d0f920c7d241d50ed1f1086d2f3edb4
zuguangz97 <zuguangzhu3@gmail.com>
2024-04-12 15:18:29 -0700

    set up OpenFeign and config to do inter microservice call by RPC not reset ip + port, change ProductService to legal name by OpenFeign

--- OrderService/src/main/java/net/nvsoftware/OrderService/entity/OrderEntity.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/entity/OrderEntity.java
@@ -4,14 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.Instant;
 import java.util.Objects;
 
 @Entity
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -19,19 +20,17 @@ public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
     private long productId;
-    private long productQuantity;
+    private long quantity;
     private long totalAmount;
-    private Instant orderTime;
+    private Instant orderDate;
     private String orderStatus;
-
     public static class OrderEntityBuilder {
         public OrderEntity build() {
-            Objects.requireNonNull(orderTime, "Order time cannot be null");
+            Objects.requireNonNull(orderDate, "Order time cannot be null");
             Objects.requireNonNull(orderStatus, "Order status cannot be null");
 
-            return new OrderEntity(id, productId, productQuantity, totalAmount, orderTime, orderStatus);
+            return new OrderEntity(id, productId, quantity, totalAmount, orderDate, orderStatus);
         }
     }
 }
--- OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package net.nvsoftware.OrderService.service;
 
 import lombok.extern.log4j.Log4j2;
+import net.nvsoftware.OrderService.client.ProductServiceFeignClient;
 import net.nvsoftware.OrderService.entity.OrderEntity;
 import net.nvsoftware.OrderService.model.OrderRequest;
 import net.nvsoftware.OrderService.repository.OrderRepository;
@@ -14,21 +15,34 @@ import java.time.Instant;
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private ProductServiceFeignClient productServiceFeignClient;
 
     @Override
     public long placeOrder(OrderRequest orderRequest) {
         log.info("Start: OrderService placeOrder");
+
+        // Check if quantity is 0 or less, as 'long' cannot be null
+        if (orderRequest.getQuantity() <= 0) {
+            log.error("Invalid quantity: " + orderRequest.getQuantity());
+            throw new IllegalArgumentException("Quantity must be greater than zero.");
+        }
+
         //use OrderService to create a OrderEntity with Status Created, ORM JPA save to database
         OrderEntity orderEntity = OrderEntity.builder()
                 .productId(orderRequest.getProductId())
-                .productQuantity(orderRequest.getQuantity())
+                .quantity(orderRequest.getQuantity())
                 .totalAmount(orderRequest.getTotalAmount())
-                .orderTime(Instant.now())
+                .orderDate(Instant.now())
                 .orderStatus("CREATED")
                 .build();
         orderRepository.save(orderEntity);
         log.info("Process: OrderService placeOrder save OrderEntity with orderId: " + orderEntity.getId());
         //call ProductService to check product quantity, if ok reduce it, else throw not enough
+        productServiceFeignClient.reduceQuantity(orderEntity.getProductId(), orderEntity.getQuantity());
+        log.info("Process: OrderService placeOrder FeignCall ProductService reduceQuantity");
+
+
         //call PaymentService to charge, if success, make order paid,else cancelled
         log.info("End: OrderService placeOrder");
         return orderEntity.getId();
