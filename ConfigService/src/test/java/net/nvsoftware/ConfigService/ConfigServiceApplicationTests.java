35499ade4d83773f9ecefffc99f64c07a2674091
zuguangz97 <zuguangzhu3@gmail.com>
2024-04-12 13:26:38 -0700

    inti and setup ConfigService

---
+++ ConfigService/src/test/java/net/nvsoftware/ConfigService/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.ConfigService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
