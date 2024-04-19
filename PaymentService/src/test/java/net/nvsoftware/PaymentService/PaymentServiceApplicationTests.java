d2a23d7e926f2d90e1aca065d151fb3846593d05
zuguangz97 <zuguangzhu3@gmail.com>
2024-04-18 17:31:41 -0700

    Init PaymentService

---
+++ PaymentService/src/test/java/net/nvsoftware/PaymentService/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nvsoftware.PaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
