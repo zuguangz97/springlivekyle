fefbfc97f32b7b82791d8903892f3d9202ef76d3
zuguangz97 <zuguangzhu3@gmail.com>
2024-06-03 15:24:04 -0700

    add ProductResponse into OrderResponse, use Restful API call to getProductResponseById

--- OrderService/src/main/java/net/nvsoftware/OrderService/model/OrderResponse.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/model/OrderResponse.java
@@ -17,4 +17,16 @@ public class OrderResponse {
     private Instant orderDate;
     private long totalAmount;
     private String orderStatus;
+    private ProductResponse productResponse;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class ProductResponse {
+        private long id;
+        private String name;
+        private long price;
+        private long quantity;
+    }
 }
--- OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
+++ OrderService/src/main/java/net/nvsoftware/OrderService/service/OrderServiceImpl.java
@@ -10,6 +10,7 @@ import net.nvsoftware.OrderService.model.PaymentRequest;
 import net.nvsoftware.OrderService.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Instant;
 
@@ -22,6 +23,8 @@ public class OrderServiceImpl implements OrderService {
     private ProductServiceFeignClient productServiceFeignClient;
     @Autowired
     private PaymentServiceFeignClient paymentServiceFeignClient;
+
+    private RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public long placeOrder(OrderRequest orderRequest) {//TODO:Transaction
@@ -77,11 +80,18 @@ public class OrderServiceImpl implements OrderService {
         log.info("Start: OrderService getOrderDetailById");
         OrderEntity orderEntity = orderRepository.findById(orderId)
                         .orElseThrow(() -> new RuntimeException(" OrderService getOrderById: Order Not Found With Id"));
+
+        OrderResponse.ProductResponse productResponse = restTemplate.getForObject(
+                "http://localhost:8001/product/" + orderEntity.getProductId(),
+                OrderResponse.ProductResponse.class
+        );
+
         OrderResponse orderResponse = OrderResponse.builder()
                 .orderId(orderEntity.getId())
                 .orderDate(orderEntity.getOrderDate())
                 .orderStatus(orderEntity.getOrderStatus())
                 .totalAmount(orderEntity.getTotalAmount())
+                .productResponse(productResponse)
                 .build();
         log.info("End: OrderService getOrderDetailById");
         return orderResponse;
